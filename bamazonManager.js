ea1bb754ddca6d368d5ec156c431d911be94d540
Rainbo <jesselyn.jackson@gmail.com>
2019-08-22 23:11:19 -0400

    Modified ReadMe. Created a start up inquirer and viewAll function.

--- bamazonManager.js
+++ bamazonManager.js
@@ -7,3 +7,48 @@ user: 'root',
 password: 'Atlanta83',
 database: 'bamazonDB'
 });
+
+connection.connect(function (err) {
+    if(err) throw err;
+    
+    inquirer
+    .prompt([
+    {
+        name: "startMenu",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add  a New Product"]
+    }
+    ])
+    .then((response) => {
+    switch (response.startMenu) {
+        case "View Products for Sale":
+            viewAll();
+            break;
+        case "View Low Inventory":
+            viewLow();
+            break;
+        case "Add to Inventory":
+            incInventory();
+            break;  
+        default:
+            addNewProduct();
+            break;
+    }
+    })
+    .catch((err) => {
+        console.error(err.message);
+    });
+});
+
+function viewAll(){
+    connection.query('SELECT * FROM products', function(err, res) {
+        if(err) throw err;
+        
+        for (let i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].item_id + "\nProduct: " + res[i].product_name + "\nDepartment: " + res[i].department_name + "\nPrice: " + res[i].price + "\nQuantity: " + res[i].stock_quantity + "\n");
+        }
+
+        
+    });
+}
